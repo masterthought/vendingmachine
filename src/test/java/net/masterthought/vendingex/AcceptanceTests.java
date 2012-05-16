0102d3f674a9c2f4ce70678b3dd32abb8bac6ba8
antonymarcano <antony@antonymarcano.com>
2012-05-16 17:35:19 +0100

    We don't want to run any scenarios that are marked as @todo

--- src/test/java/net/masterthought/vendingex/AcceptanceTests.java
+++ src/test/java/net/masterthought/vendingex/AcceptanceTests.java
@@ -4,6 +4,6 @@ import cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(format = {"pretty", "json:target/cucumber.json"})
+@Cucumber.Options(format = {"pretty", "json:target/cucumber.json"}, tags = "~@todo")
 public class AcceptanceTests {
 }
